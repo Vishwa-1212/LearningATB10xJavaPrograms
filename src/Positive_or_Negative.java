f2873d142f8d652392abf7e07e374f22f504a20a
Vishwa-1212 <vs541548@gmail.com>
2025-02-01 00:14:33 +0530

    commit

---
+++ src/Positive_or_Negative.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Positive_or_Negative {
+    public static void main(String[] args) {
+        System.out.println("Enter the number:");
+        Scanner sc= new Scanner(System.in);
+        int num= sc.nextInt();
+        if(num>0){
+            System.out.println(num +" is a Positive number");
+        } else if (num==0) {
+            System.out.println(num +" is neigther Positive nor Negative");
+
+        }
+        else {
+            System.out.println(num +" is a Negative number");
+        }
+
+    }
+}
