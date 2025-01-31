ca0a6b05299ad24d4024901954b817a8b8a6d2bc
Vishwa-1212 <vs541548@gmail.com>
2025-02-01 00:56:50 +0530

    commit

---
+++ src/Vowel_or_Consonant.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Vowel_or_Consonant {
+    public static void main(String[] args) {
+        System.out.println("Enter character to check whether it vowel or consonant");
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.next().toLowerCase().charAt(0);
+        switch (ch){
+            case 'a': case 'e': case 'i': case 'o': case 'u':
+                System.out.println(ch+" is an vowel" );
+                break;
+            default:
+                System.out.println(ch+" is a consonant");
+
+        }
+
+    }
+}
