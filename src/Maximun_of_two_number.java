6c45257449a26b928824357b7398ed750a929ed3
Vishwa-1212 <vs541548@gmail.com>
2025-02-01 00:25:53 +0530

    commit

---
+++ src/Maximun_of_two_number.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Maximun_of_two_number {
+    public static void main(String[] args) {
+        System.out.println("Enter two numbers:");
+        Scanner sc=new Scanner(System.in);
+        int num1= sc.nextInt();
+        int num2= sc.nextInt();
+        if (num1>num2){
+            System.out.println(num1 +" is maximum and "+num2+" is minimum" );
+        }
+        else{
+            System.out.println(num2 +" is maximum and "+num1+" is minimum");
+        }
+    }
+}
